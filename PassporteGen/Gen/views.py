882672c89a100e58a30159a4eea751f80c325245
rha <rha2005@gmail.com>
2024-03-29 22:43:47 -0400

    Create view for the password generation

--- PassporteGen/Gen/views.py
+++ PassporteGen/Gen/views.py
@@ -24,6 +24,14 @@ def generate_password(length):
     return password
 
 # Example usage
-password_length = 12  # Change this to desired password length
-password = generate_password(password_length)
-print("Generated password:", password)
+#password_length = 12  # Change this to desired password length
+#password = generate_password(password_length)
+#print("Generated password:", password)
+
+def generate_password_view(request):
+    if request.method == 'POST':
+        length = int(request.POST.get('length', 12))  # Default length is 12 if not specified
+        password = generate_password(length)
+        return render(request, 'password_generator.html', {'password': password})
+    else:
+        return render(request, 'password_generator.html')
